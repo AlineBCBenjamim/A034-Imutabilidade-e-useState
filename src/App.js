741c6a6fd2b5814c69742cacb4b3eadc0e4a024f
Aline Carvalho <alineoak9@gmail.com>
2022-10-18 09:54:19 -0300

    exercicio pokemon

--- src/App.js
+++ src/App.js
@@ -19,22 +19,97 @@ function App() {
     type: "Electric",
     evolved: false,
     weight: 2,
-    color: 'yellow',
+    color: "yellow",
     image: 'https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png',
     id: 0
   })
+  const [pokemon1, setPokemon1] = useState({
+    name: "Charmander",
+    type: "Fire",
+    evolved: false,
+    weight: 8.5,
+    color: "Red",
+    image:"https://archives.bulbagarden.net/media/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png",
+    id:0
+  }) 
 
+  const [pokemon2, setPokemon2] = useState({
+    name: "Bulbasaur",
+    type: "Grass, Poison",
+    evolved: false,
+    weight: 6.9,
+    color: "Green",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png",
+    id: 0,
+  }); 
+
+  const [pokemon3, setPokemon3] = useState({
+    name: "Squirtle",
+    type: "Water",
+    evolved: false,
+    weight: 9,
+    color: "Blue",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png",
+    id: 0,
+  }); 
+
+  const [pokemon4, setPokemon4] = useState({
+    name: "Togepi",
+    type: "Fairy",
+    evolved: false,
+    weight: 1.5,
+    color: "White",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/6/6b/175Togepi.png/250px-175Togepi.png",
+    id: 0,
+  }); 
+
+  const [pokemon5, setPokemon5] = useState({
+    name: "Abra",
+    type: "Psychic",
+    evolved: false,
+    weight: 19.5,
+    color: "Brown",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/6/62/063Abra.png/250px-063Abra.png",
+    id: 0,
+  }); 
+
+  const [pokemon6, setPokemon6] = useState({
+    name: "Gastly",
+    type: "Ghost, Poison",
+    evolved: false,
+    weight: 0.1,
+    color: "Purple",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/c/ca/092Gastly.png/250px-092Gastly.png",
+    id: 0,
+  }); 
   // Para fazer seus próximos pokemons, crie novos estados!
 
-  return ( <>
-  <GlobalStyles/>
-    <FlexContainer>
-      {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
-      <PokemonCard/>
-      {/* Crie aqui seus próximos pokemons! */}
-    </FlexContainer>
-  </>
-    
+  return (
+    <>
+      <GlobalStyles />
+      <FlexContainer>
+        {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
+        <PokemonCard pokemon={pokemon} setPokemon={setPokemon} />
+
+        <PokemonCard pokemon={pokemon1} setPokemon={setPokemon1} />
+
+        <PokemonCard pokemon={pokemon2} setPokemon={setPokemon2} />
+
+        <PokemonCard pokemon={pokemon3} setPokemon={setPokemon3} />
+
+        <PokemonCard pokemon={pokemon4} setPokemon={setPokemon4} />
+
+        <PokemonCard pokemon={pokemon5} setPokemon={setPokemon5} />
+
+        <PokemonCard pokemon={pokemon6} setPokemon={setPokemon6} />
+        {/* Crie aqui seus próximos pokemons! */}
+      </FlexContainer>
+    </>
   );
 }
 
--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -1,24 +1,201 @@
 import React from 'react'
-import {Card, PokemonName, PokemonType, EvolveButton} from './styles'
-
+import {
+  Card,
+  PokemonName,
+  PokemonType,
+  EvolveButton
+} from './styles'
 
 
 const PokemonCard = (props) => {
+  console.log(props);
+  const evoluirPokemon = (pokemonName) => {
+    console.log("Cliquei no botão de evoluir");
 
-    const evoluirPokemon = () => {
-        console.log("Cliquei no botão de evoluir")
+    if (pokemonName === "Pichu") {
+      props.setPokemon({
+        ...props.pokemon,
+        name: "Pikachu",
+        type: "Electric",
+        evolved: true,
+        weight: 6,
+        color: "yellow",
+        image:
+          "https://archives.bulbagarden.net/media/upload/thumb/0/0d/025Pikachu.png/250px-025Pikachu.png",
+        id: 0
+      });
+    } else if (pokemonName === "Pikachu") {
+        props.setPokemon({
+          ...props.pokemon,
+          name: "Raichu",
+          type: "Electric",
+          evolved: true,
+          weight: 30,
+          color: "yellow",
+          image:
+            "https://archives.bulbagarden.net/media/upload/thumb/8/88/026Raichu.png/250px-026Raichu.png",
+          id: 0
+        });
+      }else if(pokemonName === "Charmander"){
+        props.setPokemon({
+          ...props.pokemon1,
+          name: "Charmeleon",
+          type: "Fire",
+          evolved: true,
+          weight: 19,
+          color: "Red",
+          image:
+            "https://archives.bulbagarden.net/media/upload/thumb/4/4a/005Charmeleon.png/250px-005Charmeleon.png",
+          id: 0
+        });
+      }else if (pokemonName === "Charmeleon") {
+        props.setPokemon({
+          ...props.pokemon1,
+          name: "Charizard",
+          type: "Fire, Flying",
+          evolved: true,
+          weight: 90.5,
+          color: "Red",
+          image:
+            "https://archives.bulbagarden.net/media/upload/thumb/7/7e/006Charizard.png/250px-006Charizard.png",
+          id: 0
+        });
+        }else if (pokemonName === "Bulbasaur") {
+          props.setPokemon({
+            ...props.pokemon2,
+            name: "Ivysaur",
+            type: "Grass, Poison",
+            evolved: true,
+            weight: 13,
+            color: "Green",
+            image: "https://archives.bulbagarden.net/media/upload/thumb/7/73/002Ivysaur.png/250px-002Ivysaur.png",              
+            id: 0,
+          });
+          }else if (pokemonName === "Ivysaur") {
+            props.setPokemon({
+              ...props.pokemon2,
+              name: "Venusaur",
+              type: "Grass, Poison",
+              evolved: true,
+              weight: 100,
+              color: "Green",
+              image:
+                "https://archives.bulbagarden.net/media/upload/thumb/a/ae/003Venusaur.png/250px-003Venusaur.png",
+              id: 0,
+            });
+            }else if (pokemonName === "Squirtle") {
+              props.setPokemon({
+                ...props.pokemon3,
+                name: "Wartortle",
+                type: "Water",
+                evolved: true,
+                weight: 22.5,
+                color: "Blue",
+                image:
+                  "https://archives.bulbagarden.net/media/upload/thumb/0/0c/008Wartortle.png/250px-008Wartortle.png",
+                id: 0,
+              });
+              }else if (pokemonName === "Wartortle") {
+                props.setPokemon({
+                  ...props.pokemon3,
+                  name: "Blastoise",
+                  type: "Water",
+                  evolved: true,
+                  weight: 85.5,
+                  color: "Blue",
+                  image:
+                    "https://archives.bulbagarden.net/media/upload/thumb/0/02/009Blastoise.png/250px-009Blastoise.png",
+                  id: 0,
+                });
+                }else if (pokemonName === "Togepi") {
+                  props.setPokemon({
+                    ...props.pokemon4,
+                    name: "Togetic",
+                    type: "Fairy, Flying",
+                    evolved: true,
+                    weight: 3.2,
+                    color: "White",
+                    image:
+                      "https://archives.bulbagarden.net/media/upload/thumb/1/11/176Togetic.png/250px-176Togetic.png",
+                    id: 0,
+                  });
+                  }else if (pokemonName === "Togetic") {
+                    props.setPokemon({
+                      ...props.pokemon4,
+                      name: "Togekiss",
+                      type: "Fairy, Flying",
+                      evolved: true,
+                      weight: 38,
+                      color: "White",
+                      image:
+                        "https://archives.bulbagarden.net/media/upload/thumb/8/87/468Togekiss.png/250px-468Togekiss.png",
+                      id: 0,
+                    });
+                    }else if (pokemonName === "Abra") {
+                      props.setPokemon({
+                        ...props.pokemon5,
+                        name: "Kadabra",
+                        type: "Psychic",
+                        evolved: true,
+                        weight: 56.5,
+                        color: "Brown",
+                        image:
+                          "https://archives.bulbagarden.net/media/upload/thumb/9/97/064Kadabra.png/250px-064Kadabra.png",
+                        id: 0,
+                      });
+                      }else if (pokemonName === "Kadabra") {
+                        props.setPokemon({
+                          ...props.pokemon5,
+                          name: "Alakazam",
+                          type: "Psychic",
+                          evolved: true,
+                          weight: 48,
+                          color: "Brown",
+                          image:
+                            "https://archives.bulbagarden.net/media/upload/thumb/c/cc/065Alakazam.png/250px-065Alakazam.png",
+                          id: 0,
+                        });
+                        }else if (pokemonName === "Gastly") {
+                          props.setPokemon({
+                            ...props.pokemon6,
+                            name: "Haunter",
+                            type: "Ghost, Poison",
+                            evolved: true,
+                            weight: 0.1,
+                            color: "Purple",
+                            image:
+                              "https://archives.bulbagarden.net/media/upload/thumb/6/62/093Haunter.png/250px-093Haunter.png",
+                            id: 0,
+                          });
+                          }else if (pokemonName === "Haunter") {
+                            props.setPokemon({
+                              ...props.pokemon6,
+                              name: "Gengar",
+                              type: "Ghost, Poison",
+                              evolved: true,
+                              weight: 40.5,
+                              color: "Purple",
+                              image:
+                                "https://archives.bulbagarden.net/media/upload/thumb/c/c6/094Gengar.png/250px-094Gengar.png",
+                              id: 0,
+                            });
+                          }
+      
     }
-    
-  return (
-    <Card color={}>
-        <img src={} alt={`Pokemon`}/>
-        <PokemonName>{}</PokemonName>
-        <PokemonType>{}</PokemonType>
-        <p>{}kg</p>
+  
 
-        <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
+  return (
+    <Card color={props.pokemon.color}>
+      <img src={props.pokemon.image} alt={`Pokemon`} />
+      <PokemonName>{props.pokemon.name}</PokemonName>
+      <PokemonType>{props.pokemon.type}</PokemonType>
+      <p>{props.pokemon.weight}kg</p>
+
+      <EvolveButton onClick={() => evoluirPokemon(props.pokemon.name)}>
+        Evoluir!
+      </EvolveButton>
     </Card>
-  )
-}
+  );
+};
 
 export default PokemonCard
